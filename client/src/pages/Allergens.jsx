c630a473f6bf377f0d9c2555a61971ddfae4e334
DominikaDuralek <dominikaduralek@gmail.com>
2024-06-12 23:55:38 +0200

    Saving allergens to localStorage and fetching products from database

--- client/src/pages/Allergens.jsx
+++ client/src/pages/Allergens.jsx
@@ -1,69 +1,105 @@
+import { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
+
 function Allergens() {
+    useEffect(() => { checkLocalStorage(); allergensToString(); },[]);
+
+    // Dynamic string value with all checked allergens
+    const [allergens, setAllergens] = useState('');
+
     // Checking correct boxes on load
     const checkLocalStorage = () => {
         const array = document.getElementsByTagName("input");
-        console.log("check");
+        // Check if values exist in localStorage and check the correct boxes
+        for (let i = 0; i < array.length; i++) {
+            if (localStorage.getItem(array[i].id) !== null) {
+                array[i].checked = true;
+            } else {
+                array[i].checked = false;
+            }
+        }
     }
 
-    window.onload = checkLocalStorage();
+    const allergensToString = () => {
+        // Add all checked allergens to a string and return
+        const array = document.getElementsByTagName("input");
+        let allergensString = "";
+        for (let i = 0; i < array.length; i++) {
+            if (array[i].checked == true) {
+                // If last item reached
+                if (i == array.length - 1) allergensString = allergensString.concat(array[i].id);
+                else allergensString = allergensString.concat(array[i].id + ", ");
+            }
+        }
+
+        if (allergensString == "") setAllergens("nie wybrano");
+        else setAllergens(allergensString);
+    }
 
     // Saving an allergen to localStorage
     const setAllergen = (allergen) => {
         if(localStorage.getItem(allergen) === null) localStorage.setItem(allergen, allergen);
         else localStorage.removeItem(allergen);
+
+        // Update the info about chosen allergens
+        allergensToString();
     }
 
     return (
         <div className="allergens">
-            Alergeny
+            <h1 className="allergens__title">Lista alergenów</h1>
             <div className="allergens__checkboxes">
                 <div className="allergens__checkbox-group">
                     <div className="allergens__checkbox">
-                        <input type="checkbox" id="alergen1" value="alergen1" onChange={() => setAllergen("allergen1")} />
+                        <input type="checkbox" id="alergen1" value="alergen1" onChange={() => setAllergen("alergen1")} />
                         <label htmlFor="allergen1">Alergen 1</label>
                     </div>
                     <div className="allergens__checkbox">
-                        <input type="checkbox" id="alergen2" value="alergen2" onChange={() => setAllergen("allergen2")} />
-                        <label htmlFor="allergen2">Alergen 2</label>
+                        <input type="checkbox" id="alergen2" value="alergen2" onChange={() => setAllergen("alergen2")} />
+                        <label htmlFor="alergen2">Alergen 2</label>
                     </div>
                     <div className="allergens__checkbox">
-                        <input type="checkbox" id="alergen3" value="alergen3" onChange={() => setAllergen("allergen3")} />
-                        <label htmlFor="allergen3">Alergen 3</label>
-                    </div>
-                </div>
-
-                <div className="allergens__checkbox-group">
-                    <div className="allergens__checkbox">
-                        <input type="checkbox" id="alergen4" value="alergen4" />
-                        <label htmlFor="allergen4">Alergen 4</label>
-                    </div>
-                    <div className="allergens__checkbox">
-                        <input type="checkbox" id="alergen5" value="alergen5" />
-                        <label htmlFor="allergen5">Alergen 5</label>
-                    </div>
-                    <div className="allergens__checkbox">
-                        <input type="checkbox" id="alergen6" value="alergen6" />
-                        <label htmlFor="allergen6">Alergen 6</label>
+                        <input type="checkbox" id="alergen3" value="alergen3" onChange={() => setAllergen("alergen3")} />
+                        <label htmlFor="alergen3">Alergen 3</label>
                     </div>
                 </div>
 
                 <div className="allergens__checkbox-group">
                     <div className="allergens__checkbox">
-                        <input type="checkbox" id="alergen7" value="alergen7" />
-                        <label htmlFor="allergen7">Alergen 7</label>
+                        <input type="checkbox" id="alergen4" value="alergen4" onChange={() => setAllergen("alergen4")} />
+                        <label htmlFor="alergen4">Alergen 4</label>
                     </div>
                     <div className="allergens__checkbox">
-                        <input type="checkbox" id="alergen8" value="alergen8" />
-                        <label htmlFor="allergen8">Alergen 8</label>
+                        <input type="checkbox" id="alergen5" value="alergen5" onChange={() => setAllergen("alergen5")} />
+                        <label htmlFor="alergen5">Alergen 5</label>
                     </div>
                     <div className="allergens__checkbox">
-                        <input type="checkbox" id="alergen9" value="alergen9" />
-                        <label htmlFor="allergen9">Alergen 9</label>
+                        <input type="checkbox" id="alergen6" value="alergen6" onChange={() => setAllergen("alergen6")} />
+                        <label htmlFor="alergen6">Alergen 6</label>
+                    </div>
+                </div>
+
+                <div className="allergens__checkbox-group">
+                    <div className="allergens__checkbox">
+                        <input type="checkbox" id="alergen7" value="alergen7" onChange={() => setAllergen("alergen7")} />
+                        <label htmlFor="alergen7">Alergen 7</label>
+                    </div>
+                    <div className="allergens__checkbox">
+                        <input type="checkbox" id="alergen8" value="alergen8" onChange={() => setAllergen("alergen8")} />
+                        <label htmlFor="alergen8">Alergen 8</label>
+                    </div>
+                    <div className="allergens__checkbox">
+                        <input type="checkbox" id="alergen9" value="alergen9" onChange={() => setAllergen("alergen9")} />
+                        <label htmlFor="alergen9">Alergen 9</label>
                     </div>
                 </div>
             </div>
+            <div className="allergens__chosen">
+                <p>Twoje alergeny: {allergens}</p>
+            </div>
             <div className="allergens__disclaimer">
                 <p>Wybór zostanie zapamiętany do momentu wyczyszczenia przeglądarki.</p>
+                <p><Link to="/">Przejdź do Skanera</Link></p>
             </div>
         </div>
     )
--- client/src/pages/Scanner.jsx
+++ client/src/pages/Scanner.jsx
@@ -1,7 +1,56 @@
+// Show info about product in scanner__product component
+const showProductInfo = () => {
+    const code = document.getElementById("code").value;
+    // Fetch the correct product
+    window.fetch("https://world.openfoodfacts.org/api/v3/product/" + code + ".json")
+        .then(function(response){
+            return response.json();
+        }).then(function(json){
+            console.log("name: " + json.product.product_name);
+            console.log("code: " + json.code);
+            console.log("allergens: " + json.product.allergens);
+        }).catch(function(error) {
+            console.log(error);
+        });
+
+    document.querySelector(".scanner__camera").style.display = "none";
+    document.querySelector(".scanner__code").style.display = "none";
+    document.querySelector(".scanner__product").style.display = "flex";
+}
+
+// Hide info about product in scanner__product component
+const hideProductInfo = () => {
+    document.querySelector(".scanner__camera").style.display = "flex";
+    document.querySelector(".scanner__code").style.display = "flex";
+    document.querySelector(".scanner__product").style.display = "none";
+}
+
 function Scanner() {
     return (
         <div className="scanner">
-            Skaner
+            <h1 className="scanner__title">Wprowadź produkt</h1>
+            <div className="scanner__container">
+                <div className="scanner__camera">
+                    <h2>Opcja 1</h2>
+                    <h2>Skaner dostępny tylko dla urządzeń mobilnych</h2>
+                    <div className="scanner__camera-icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="0.5" stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
+                        </svg>
+                    </div>
+                </div>
+                <div className="scanner__code">
+                    <h2>Opcja 2</h2>
+                    <h2>Wprowadź kod produktu</h2>
+                    <input type="text" className="scanner__code-input" id="code" />
+                    <button className="scanner__code-submit" onClick={showProductInfo} >Wyszukaj</button>
+                </div>
+
+                <div className="scanner__product">
+                    <p>Informacje o produkcie</p>
+                    <button className="scanner__product__button-back" onClick={hideProductInfo} >Wyszukaj kolejny produkt</button>
+                </div>
+            </div>
         </div>
     )
 }
