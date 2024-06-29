beba6d97c359c0ff54e683a2bbefbb7b4b8fec69
DominikaDuralek <dominikaduralek@gmail.com>
2024-06-30 00:15:46 +0200

    Developing the UI

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,20 +2,18 @@ import { useContext } from "react";
 import { Link } from 'react-router-dom';
 
 function Navbar() {
-    return (
-      <div className='navbar'>
-        <div className="container">
-          <div className="logo">
-            Logo
-          </div>
-          <div className="links">
-            <Link className="link" to="/"><h6>Skaner</h6></Link>
-            <Link className="link" to="/allergens"><h6>Alergeny</h6></Link>
-            <Link className="link" to="/info"><h6>Informacje</h6></Link>
-          </div>
-        </div>
+  return (
+    <div className='navbar'>
+      <div className="logo">
+        <img src="../public/logo.png" alt="logo" />
       </div>
-    );
-  }
-  
-  export default Navbar;
+      <div className="links">
+        <Link className="link" to="/"><h6>Skaner</h6></Link>
+        <Link className="link" to="/allergens"><h6>Alergeny</h6></Link>
+        <Link className="link" to="/info"><h6>Informacje</h6></Link>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
